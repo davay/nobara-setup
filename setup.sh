b836d8a5fac45561c741a1eab2f915a136681c0a
Devin Lim <davay@devinl.im>
2025-01-02 14:22:10 -0800

    init dotfiles

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 sudo dnf install -y ansible
 git clone https://github.com/davay/nobara-setup.git ~/Repos/nobara-setup
-ansible-playbook ./playbooks/packages.yml
+ansible-playbook ./playbooks/init.yml
+ansible-playbook ./playbooks/dotfiles.yml
