f026aab9b5726feed839db5657a343d93b869965
Devin Lim <davay@devinl.im>
2025-01-02 16:21:24 -0800

    disable gpg check for synergy

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo dnf install -y ansible
-git clone https://github.com/davay/nobara-setup.git ~/Repos/nobara-setup
-ansible-playbook ./playbooks/init.yml
-ansible-playbook ./playbooks/dotfiles.yml
+sudo dnf install -y ansible && \
+git clone https://github.com/davay/nobara-setup.git ~/Repos/nobara-setup && \
+ansible-playbook ./playbooks/init.yml && \
+ansible-playbook ./playbooks/dotfiles.yml 
