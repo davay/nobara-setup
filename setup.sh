42532130795e56ed4d54285007d1b0acb8b63ffd
Devin Lim <davay@devinl.im>
2025-01-04 03:00:49 -0800

    firewalld

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 sudo dnf install -y ansible ; \
 git clone https://github.com/davay/nobara-setup.git ~/Repos/nobara-setup ; \
 ansible-playbook ./playbooks/init.yml && \
-ansible-playbook ./playbooks/dotfiles.yml 
+ansible-playbook ./playbooks/dotfiles.yml && \
+ansible-playbook ./playbooks/firewalld.yml
