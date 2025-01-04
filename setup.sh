c25aba2836db5eb48f71a01cf222d7bb21c3d93e
Devin Lim <davay@devinl.im>
2025-01-04 11:45:45 -0800

    ask-become-pass

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 
 sudo dnf install -y ansible ; \
 git clone https://github.com/davay/nobara-setup.git ~/Repos/nobara-setup ; \
-ansible-playbook ./playbooks/init.yml && \
-ansible-playbook ./playbooks/dotfiles.yml && \
-ansible-playbook ./playbooks/firewalld.yml
+ansible-playbook ./playbooks/init.yml --ask-become-pass && \
+ansible-playbook ./playbooks/dotfiles.yml --ask-become-pass && \
+ansible-playbook ./playbooks/firewalld.yml --ask-become-pass
