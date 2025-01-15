8665a399e8a669a5c01f754f03743886e33ee75b
Devin Lim <davay@devinl.im>
2025-01-15 01:10:16 -0800

    github self-hosted runner

--- servers.sh
+++ servers.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ansible-playbook ./playbooks/containers.yml --ask-become-pass && \
-ansible-playbook ./playbooks/nginx.yml --ask-become-pass
+ansible-playbook ./playbooks/nginx.yml --ask-become-pass && \
+ansible-playbook ./playbooks/github.yml 
